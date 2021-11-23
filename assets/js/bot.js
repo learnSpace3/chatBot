f7e32ab46ff3c4d6e5e7993f56d6823af089f1b9
temujin469 <ttemujin469@gmail.com>
2021-11-23 22:05:53 +0800

    anhnii huwilbar v0.1

---
+++ assets/js/bot.js
@@ -0,0 +1,17 @@
+function getBotResponse(userValue){
+    if(userValue == 'sain uu'){
+        return 'sain sain uu';
+    }else if( userValue == 'yu bainda'){
+        return 'hicheele hiigel baijinda';
+    }
+    else if(userValue == 'bayrtai'){
+        return 'bayrtai saihn ammrai';
+    }
+    else if(userValue == 'facebook'){
+        return '<a href="https://www.facebook.com/profile.php?id=100034295059325">temuujingiin face</a>';
+    }
+    else{
+        return 'asuuh zvil bain uu';
+    }
+
+}
